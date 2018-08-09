4840c9c661017023b611e505576687af8e6e9fc1
Joanna Gora <joanna.gora@protonmail.com>
2018-08-09 14:02:52 +0100

    Created Club and tested Club.

--- src/main/java/Weapons/Club.java
+++ src/main/java/Weapons/Club.java
@@ -1,4 +1,22 @@
 package Weapons;
 
+import Enemies.Enemy;
+
 public class Club {
+    private int damage;
+    private String name;
+
+    public Club() {
+        this.damage = 10;
+        this.name = "Spikey";
+    }
+
+    public void attack(Enemy enemy){
+        int newHealth = enemy.getHealth() - this.damage;
+        enemy.setHealth(newHealth);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
